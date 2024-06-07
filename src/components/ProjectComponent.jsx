741795f49c6ce9f897a3f0dc791f476509aeabfa
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-06-07 21:16:26 +0800

    Finished Project Section

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -8,26 +8,29 @@ const ProjectComponent = () => {
     <>
       {projectData.map((proj, i) =>{
         return <div className=' mt-16 flex'>
-          <div className='w-[50%] border border-red-100'>
-            <img src={proj.image} alt="" className='w-[50%]'/>
+          <div className={`w-[60%] ${proj.value % 2 === 0 ? 'order-1' : ''}`}>
+            <a href={proj.link} target='_blank'>
+              <img src={proj.image} alt="" className=' brightness-50 hover:brightness-100'/> 
+            </a>
           </div>
-          <div className='border'>
-            <div className='border border-green-300 text-end'>
+          <div className={`w-[40%] flex flex-col gap-6 ${proj.value % 2 === 0 ? 'mr-4' : 'ml-4'}`}>
+            <div className={`${proj.value % 2 === 0 ? 'text-left' : 'text-end'}`}>
               <p className=' font-mono text-highlight text-[1.2rem]'>Featured Project</p>
-              <p className=' text-headers font-Poppins text-end text-[1.7rem]'>
+              <p className=' text-headers font-Poppins text-[2rem] font-semibold'>
                 {proj.title}</p>
             </div>
-            <div>
+            <div className={`bg-[#112240] p-8 ${proj.value % 2 === 0 ? 'text-left' : 'text-end'}`}>
               <p className='text-text font-Poppins'>{proj.description}</p>
             </div>
             <div>
-              <p className='text-text font-mono text-end'>{proj.tech}</p>
+              <p className={`text-text font-mono ${proj.value % 2 === 0 ? 'text-left' : 'text-end'}`}>
+                {proj.tech}</p>
             </div>
-            <div className='flex justify-end gap-4'>
-              <a href="">
+            <div className={`flex gap-4 ${proj.value % 2 === 0 ? ' justify-start' : 'justify-end'}`}>
+              <a href="https://github.com/novice-is-me" target='_blank'>
                 <GitHubIcon style={{color:'#8892b0'}}/>
               </a>
-              <a href="">
+              <a href={proj.link} target='_blank'>
                 <ExitToAppIcon style={{color:'#8892b0'}}/>
               </a>
             </div>
--- src/constant.js
+++ src/constant.js
@@ -52,37 +52,43 @@ export const projectData = [
         title: "Student System",
         description: "Simple student system consists of create, read, update and delete. Hosted at netlify.",
         tech: "React JS, JSON, Axios, Tailwind",
-        link: 'https://react-student-system.netlify.app/'
+        link: 'https://react-student-system.netlify.app/',
+        value: 1
     },{
         image: messenger,
         title: "Messenger Clone",
         description: "Clone of desktop messenger with the ability to send message on each group chats.",
         tech: "Javascript, React JS, Tailwind, MaterialUI",
-        link: 'https://clone-messenger.netlify.app/message'
+        link: 'https://clone-messenger.netlify.app/',
+        value: 2
     },{
         image: gym,
         title: "PowerLab Gym",
         description: "A landing page website of PowerLab consists of the services they offer and their specific prices.",
         tech: "React JS, Tailwind",
-        link: 'https://powerlab-gym.netlify.app/'
+        link: 'https://powerlab-gym.netlify.app/',
+        value: 3
     },{
         image: jokes,
         title: "CompAPI",
         description: "A compilation of jokes and facts from free API's",
         tech: "HTML, CSS, JavaScript, Fetch, JQuery, Bootstrap",
-        link: 'https://novice-is-me.github.io/Free-Api/'
+        link: 'https://novice-is-me.github.io/Free-Api/',
+        value: 4
     },{
         image: qoutes,
         title: "Qoute Generator",
         description: "A static qoute generator that randomly displays qoutes from famous individual.",
         tech: "HTML, CSS, JavaScript, Bootstrap",
-        link: 'https://novice-is-me.github.io/Quote-Generator/'
+        link: 'https://novice-is-me.github.io/Quote-Generator/',
+        value: 5
     },{
         image: food,
         title: "Food Recipe Blog",
         description: "A static landing page website displaying all featured and famous recipe across the Asia.",
         tech: "HTML, CSS, Bootstrap",
-        link: 'https://novice-is-me.github.io/Food-Recipe-Blog/' 
+        link: 'https://novice-is-me.github.io/Food-Recipe-Blog/' ,
+        value: 6
     }   
 ]
 
